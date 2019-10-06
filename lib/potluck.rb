bab12972dade2abac444443179c7f9b89165b931
Becky Robran <rerobran@gmail.com>
2019-10-06 13:46:48 -0600

    added potluck menu method

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,13 +1,30 @@
 class Potluck
-  attr_reader :date, :dishes, :menu
+  attr_reader :date, :dishes, :menu, :category_array
   def initialize(date)
     @date = date
     @dishes = []
+    @category_array
     @menu = Hash.new
   end
   def add_dish(dish)
-    dishes << dish.category
-    dishes
+    dishes << dish
+    dishes.last
+  end
+  def get_all_from_category(category)
+    @category_array = dishes.find_all do |dish|
+      dish.category == category
+    end
+  end
+  def potluck_menu
+      @menu = Hash.new {|hash, key| hash[key] = []}
+      @dishes.each do |dish|
+        @menu[dish.category] << dish
+      end
+      @menu 
+  end
+
+  def ratio(category)
+
   end
 
 end
